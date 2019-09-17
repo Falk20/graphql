cf44fd3537bd407ecf4fb1f38c1c1ac09a19e2d9
falk <petruk0908@gmail.com>
2019-09-17 03:21:28 +0300

    backend part ready

--- server/app.js
+++ server/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.use('graphql', graphqlHTTP({
-    
+mongoose.connect("mongodb+srv://falkal:0gbf0x2r@cluster0-8b9vu.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true }, ()=>{
+    console.log('connected to database');
+});
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
 }));
+
+
 
 app.listen(4000, ()=>{
     console.log("now listening for request on port 4000");
